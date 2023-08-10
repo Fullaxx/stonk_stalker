9891522ba041a58c4f748ca8cad6c668a178cac9
Brett Kuskie <fullaxx@gmail.com>
2023-08-10 03:30:12 -0600

    Switch time p element to div

--- gen_html.py
+++ gen_html.py
@@ -84,7 +84,7 @@ def gen_html_body():
 	html = '<body>'
 	html += '<center>'
 	html += '<h2>Stonk Stalker</h2>'
-	html += '<h3><p id="time"></p></h3>'
+	html += '<h3><div id="time"></div></h3>'
 	for tbl in tables_list:
 		html += gen_html_table(tbl)
 	html += '<a href="https://github.com/Fullaxx/stonk_stalker">Source Code on GitHub</a>'
