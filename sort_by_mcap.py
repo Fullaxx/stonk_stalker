1e1b35a3b98c4eb4277e0c05d936615aeec83472
Brett Kuskie <fullaxx@gmail.com>
2023-08-12 03:44:32 -0600

    Add sort_by_mcap.py

---
+++ sort_by_mcap.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# pip3 install yfinance
+
+import os
+import sys
+import yfinance as yf
+
+def bailmsg(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+	exit(1)
+
+def sort_list(symb_list):
+	mcap_list = []
+	mcap_dict = {}
+	for symb in symb_list:
+		res = yf.Ticker(symb)
+		if 'marketCap' not  in res.info:
+			print(f'{symb} has no marketCap object')
+			exit(0)
+		mcap = res.info['marketCap']
+		mcap_list.append(mcap)
+		mcap_dict[str(mcap)] = symb
+	mcap_list.sort(reverse=True)
+#	print(mcap_list)
+	for m in mcap_list:
+		symb = mcap_dict[str(m)]
+#		print(f'{symb}: {m}')
+		print(f'{symb}', end=',')
+
+if __name__ == '__main__':
+	symbols_env = os.getenv('SYMBOLS')
+	if symbols_env is None: bailmsg('Set SYMBOLS')
+
+	symb_list = symbols_env.split(',')
+	sort_list(symb_list)
